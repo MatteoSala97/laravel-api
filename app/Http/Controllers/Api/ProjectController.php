84a70d0bce6762226399963ec7f8a7140e82facd
Matteo Sala <matteo111997@gmail.com>
2024-04-08 18:08:20 +0200

    feat: logic to search for slug

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -13,11 +13,27 @@ class ProjectController extends Controller
     public function index(){
 
         //eager loading
-        $posts = Project::with('category', 'tags')-> paginate(3);
+        $project = Project::with('category', 'tags')-> paginate(3);
 
         return response()->json([
             'success'=> true,
-            'projects' => $posts
+            'projects' => $project
         ]);
+    }
+
+    public function show($slug){
+        $project = Project::with('category', 'tags')-> where('slug', $slug)->first();
+
+        if($project){
+            return response()->json([
+                'success'=> true,
+                'projects' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success'=> false,
+                'error' => "The project you searched for does not exist"
+            ]);
+        }
     }
 }
